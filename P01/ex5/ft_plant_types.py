79a7b747d096546cd6a1d81e49b2afc9992f707c
DiogoLiebner <diogoliebner@gmail.com>
2026-03-27 02:27:41 +0000

    finished ex4

--- P01/ex5/ft_plant_types.py
+++ P01/ex5/ft_plant_types.py
@@ -1,42 +1,42 @@
 class Plant:
     def __init__(self, name, height, old, growrate):
         self.name = name
-        self.height = int(height)
-        self.old = int(old)
+        self._height = int(height)
+        self._old = int(old)
         self.growth = 0
         self.growrate = float(growrate)
 
     def grow(self):
-        self.height += self.growrate
+        self._height += self.growrate
         self.growth += self.growrate
 
     def age(self):
-        self.old += 1
+        self._old += 1
+
+    def get_height(self):
+        return self._height
+
+    def get_age(self):
+        return self._old
 
     def set_height(self, n):
         if n < 0:
             print(f"{self.name}: Error, height can't be negative")
             print("Height update rejected\n")
         else:
-            self.height = n
-            print(f"Height updated: {self.height}cm\n")
+            self._height = n
+            print(f"Height updated: {self._height}cm\n")
 
     def set_age(self, n):
         if n < 0:
             print(f"{self.name}: Error, age can't be negative")
             print("Age update rejected\n")
         else:
-            self.old = n
-            print(f"Age updated: {self.old} days\n")
-
-    def get_height(self):
-        print(f"{self.name} Height: {self.heigh} cm")
-
-    def get_ate(self):
-        print(f"{self.name} Age: {self.old} Days")
+            self._old = n
+            print(f"Age updated: {self._old} days\n")
 
     def show(self):
-        return f"{self.name}: {self.height:.1f} cm, {self.old:.0f} days old"
+        return f"{self.name}: {self._height:.1f} cm, {self._old:.0f} days old"
 
 
 class Flower(Plant):
