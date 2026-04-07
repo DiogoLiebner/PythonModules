ebb177486e431ee330d243631cea158517b5530c
DiogoLiebner <diogoliebner@gmail.com>
2026-04-07 13:09:59 +0100

    fixed P03/ex2

--- P03/ex2/ft_coordinate_system.py
+++ P03/ex2/ft_coordinate_system.py
@@ -23,18 +23,17 @@ def main():
     print("=== Game Coordinate System ===")
 
     print("Get a first set of coordinates")
-    x, y, z = get_player_pos()
-    print(f"Got a first tuple: {x}, {y}, {z}")
-    print(f"It includes: X={x}, Y={y}, Z={z}")
-    dist = math.sqrt(x**2 + y**2 + z**2)
+    x1, y1, z1 = get_player_pos()
+    print(f"Got a first tuple: {x1}, {y1}, {z1}")
+    print(f"It includes: X={x1}, Y={y1}, Z={z1}")
+    dist = math.sqrt(x1**2 + y1**2 + z1**2)
     print(f"Distance to center: {round(dist, 4)}")
 
-# MAKE SECOND SET OF COORDINATES THE NEW CENTER
     print("Get a second set of coordinates")
-    x, y, z = get_player_pos()
-    print(f"Got a second tuple{x}, {y}, {z}")
-    print(f"It includes: X={x}, Y={y}, Z={z}")
-    dist = math.sqrt(x**2 + y**2 + z**2)
+    x2, y2, z2 = get_player_pos()
+    print(f"Got a second tuple: {x2}, {y2}, {z2}")
+    print(f"It includes: X={x2}, Y={y2}, Z={z2}")
+    dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
     print(f"Distance to center: {round(dist, 4)}")
 
 
