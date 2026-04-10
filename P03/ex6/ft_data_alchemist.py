62d5703cd209de8ddafa81b3bcfe33c7fb485764
Diogo Lima Liebner <dlima-li@c1r5s3.42porto.com>
2026-04-10 18:10:13 +0100

    finished P03

--- P03/ex6/ft_data_alchemist.py
+++ P03/ex6/ft_data_alchemist.py
@@ -2,7 +2,45 @@ import random
 
 
 def main():
+    print("=== Game Data Alchemist ===\n")
+
+    initiallist = [
+        "incineroar",
+        "Excadrill",
+        "Tyranitar",
+        "Milotic",
+        "sneasler",
+        "Sinistcha",
+    ]
+    caplist = []
+    allcaplist = []
+    scoredict = {}
+
+    print(f"Initial list of Pokemon: {initiallist}")
+    for e in initiallist:
+        if str.capitalize(e) == e:
+            caplist.append(e)
+
+    for i in initiallist:
+        allcaplist.append(str.capitalize(i))
+
+    print(f"New list of capitalized names only: {caplist}")
+    print(f"New list of all names capitalized: {allcaplist}")
+
+    for name in allcaplist:
+        scoredict[name] = random.randint(0, 252)
+
+    print(f"Score dict: {scoredict}")
+    average = sum(scoredict.values()) / len(initiallist)
+    print(f"Score average: {average:.2f}")
+
+    highscore = {}
+    for name, score in scoredict.items():
+        if score > average:
+            highscore[name] = score
+
+    print(f"High scores: {highscore}")
 
 
 if __name__ == "__main__":
-    main:
+    main()
